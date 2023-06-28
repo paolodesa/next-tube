82c1ebeb9a107896e56741c35b0dc0f57578d8e8
Paolo De Santis <paolo.desa96@gmail.com>
2023-06-28 15:42:03 +0200

    Removed node_modules folder from backend

--- next-tube-backend/transcoding-worker/node_modules/bullmq/dist/esm/scripts/addJob-8.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const addJob: {
-    name: string;
-    content: string;
-    keys: number;
-};
--- next-tube-backend/transcoding-worker/node_modules/bullmq/dist/esm/scripts/takeLock-1.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const takeLock: {
-    name: string;
-    content: string;
-    keys: number;
-};
